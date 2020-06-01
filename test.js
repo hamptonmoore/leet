9d1c688dbaa2e284a7826c2be9ba1800a7177c34
Sany <44934721+often@users.noreply.github.com>
2020-06-01 11:13:37 -0600

    Update test.js

--- test.js
+++ test.js
@@ -1,3 +1,3 @@
-import leet from './mod.js' // import the leet module
+import leet from 'https://deno.land/x/leet/mod.js' // import the leet module
 
 console.log(leet('Deno is cool')) // (|3n0 15 (00|_

16fe397c5742d8240b497f109f47b74815e69290
Sany <44934721+often@users.noreply.github.com>
2020-05-31 20:45:28 -0600

    Update mod.js
    the error looks better without the capitalized M :P

--- mod.js
+++ mod.js
@@ -1,7 +1,7 @@
 import map from './map.js'
 
 export default function leet(text) {
-	if (!text) throw 'Missing text'
+	if (!text) throw 'missing text'
 
 	return text = text.split('').map(letter => map[letter.toLowerCase()] || letter).join('')
 }
